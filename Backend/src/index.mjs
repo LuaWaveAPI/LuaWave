936c51442b9957f6a28d3786e72713ed5d1e8f4c
KhrisDV <KhrisQ@protonmail.com>
2022-05-12 12:04:02 +0200

    merge

--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -1,25 +1,37 @@
 import express from "express";
-import { deleteArticleController, getAllArticlesHandler, getArticlesHandler, postArticleController, putArticleController } from "./controllers/articlesControllers.mjs";
-import { getAllCategoriasHandler, getCategoriasHandler, postCategoriaController } from "./controllers/categoriasControllers.mjs";
+import {
+  deleteArticleController,
+  getAllArticlesHandler,
+  getArticlesHandler,
+  postArticleController,
+  putArticleController,
+} from "./controllers/articlesControllers.mjs";
+import {
+  getAllCategoriasHandler,
+  getCategoriaHandler,
+  postCategoriaController,
+} from "./controllers/categoriasControllers.mjs";
 
-const PATH_PREFIX = "/api/v0.0"
+const PATH_PREFIX = "/api/v0.0";
 const app = express();
 const port = 3000;
 
-try{
-    const jsonParser = express.json();
-//Articles
-    app.get(PATH_PREFIX+"/articles/", getAllArticlesHandler);
-    app.get(PATH_PREFIX+"/article/:id", getArticlesHandler);
-    app.post(PATH_PREFIX+"/article/",jsonParser, postArticleController);
-    app.put(PATH_PREFIX+"/article/", jsonParser, putArticleController);
-    app.delete(PATH_PREFIX+"/article", jsonParser, deleteArticleController);
-    app.get(PATH_PREFIX+"/categorias/", getAllCategoriasHandler);
-    app.get(PATH_PREFIX+"/categorias/:id", getCategoriasHandler);
+try {
+  const jsonParser = express.json();
+  //Articles
+  app.get(PATH_PREFIX + "/articles/", getAllArticlesHandler);
+  app.get(PATH_PREFIX + "/article/:id", getArticlesHandler);
+  app.post(PATH_PREFIX + "/article/", jsonParser, postArticleController);
+  app.put(PATH_PREFIX + "/article/", jsonParser, putArticleController);
+  app.delete(PATH_PREFIX + "/article", jsonParser, deleteArticleController);
+  //Categorías
+  app.get(PATH_PREFIX + "/categorias/", getAllCategoriasHandler);
+  app.get(PATH_PREFIX + "/categorias/:id", getCategoriaHandler);
+  app.post(PATH_PREFIX + "/categorias/", postCategoriaController);
 
-
-    app.listen(port,()=>{
-        console.log("Express running...")});
-}catch(err){
-    console.log("Algo va mal")
+  app.listen(port, () => {
+    console.log("Express running...");
+  });
+} catch (err) {
+  console.log("Algo va mal");
 }
