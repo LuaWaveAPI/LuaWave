2395a52af49a6bb9e1dce56eac7a2d6095e69265
bbpardo <b.b.pardo@protonmail.com>
2022-05-26 12:38:41 +0100

    Resolviendo problema con el backend

--- frontend/src/views/test/Test.jsx
+++ frontend/src/views/test/Test.jsx
@@ -11,12 +11,12 @@ function Test (){
         <h3 className="centrado">Alquiler de tablas de Skate con y sin accesorios</h3>
         <h2>Surf con accesorios</h2>
         <ListaArticulos host={urls[0]}/>
-        {/* <h2>Surf sin accesorios</h2>
+        <h2>Surf sin accesorios</h2>
         <ListaArticulos host={urls[1]}/>
         <h2>Skate con accesorios</h2>
         <ListaArticulos host={urls[2]}/>
         <h2>Skate sin accesorios</h2>
-        <ListaArticulos host={urls[3]}/> */}
+        <ListaArticulos host={urls[3]}/>
         <ArticulosCarrito/>
         <ArticuloDetallado/>
         
