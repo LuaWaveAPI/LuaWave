a2a797b5ede8a6e1a015b16c12f6ee8347c72c92
Maitlla <maitlla@gmail.com>
2022-06-16 07:53:35 +0200

    cambiado css mejor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import Carrito from "./views/Carrito/Carrito";
 import Panel from "./views/admin/paneladmin";
 
 import logoLuaWave from "./img/logoLuaWave.png";
-import carritoimg from "./img/Carrito.png";
+import carritoimg from "./img/carrito.png";
 
 
 function App() {
@@ -45,42 +45,6 @@ function App() {
 
       {/*  
       <header className="headerContainer">
-   
-        <nav className="navFondo">
-          <div className="divLuaWaveNave">
-            <Link to={"/"}>
-              <img class="logo" src={logoLuaWave} alt="logo"></img>
-            </Link>
-            <Link to={"/"}>
-              <p class="sizeP">LuaWave</p>
-            </Link>
-            <Link to={"/surf/"}>
-              <p class="sizeP">Surf</p>
-            </Link>
-            <Link to={"/skate/"}>
-              <p class="sizeP">Skate</p>
-            </Link>
-            <Link to={"/about/"}>
-              <p class="sizeP">About</p>
-            </Link>
-            <Link to={"/contact/"}>
-              <p class="sizeP">Contacto</p>
-            </Link>
-            <Link to={"/test/"}>
-              <p class="sizeP">Test</p>
-            </Link>
-            <Link to={"/carrito/"}>
-              <img class="carrito" src={carritoimg} alt="carrito"></img>
-            </Link>
-          </div>
-        </nav>
-      </header>
-     
-      <main></main>
-  
-      <footer className="footerContainer">
-       
-      </footer> 
       
       */}
 
--- frontend/src/views/surf/Surf.jsx
+++ frontend/src/views/surf/Surf.jsx
@@ -2,7 +2,9 @@ import { useState } from "react"
 import { urls } from "../../defines/defines"
 import ListaArticulos from "../../components/ListaArticulos/ListaArticulos"
 import ArticuloDetallado from "../../components/ArticuloDetallado/ArticuloDetallado"
-import "./Surf.css";
+import style from './Surf.module.css';
+import Navbar from '../../components/Navbar/Navbar.jsx';
+
 import FondoSurf from "../../img/FondoSurf.jpg";
 
 import rectanguloFondo from "../../img/rectanguloFondo.png";
@@ -40,76 +42,22 @@ function Surf() {
 
     return (
         <>
-            <main className="fondo">
-                <div>
-                    <div className="divbutton">
-                        <button className="buttonConSin" onClick={() => changeView(urls[1], "sin")}>Alquiler tablas de Surf sin neopreno</button>
-                        <button className="buttonConSin" onClick={() => changeView(urls[0], "con")}>Alquiler tablas de Surf con neopreno</button>
+
+            <div className={style.mainDividido}>
+                <div className={style.conSin}>
+                    <div>
+                        <button className={style.buttonConSin + " " + style.marginCon} onClick={() => changeView(urls[1], "sin")}>Alquiler tablas de Surf sin neopreno</button>
+                        <button className={style.buttonConSin + " " + style.marginSin} onClick={() => changeView(urls[0], "con")}>Alquiler tablas de Surf con neopreno</button>
                     </div>
-                    <div className="dividirMain">
-                        <div className="divIzq">
-                            <ListaArticulos host={pag} setArticuloFull={setArticuloFull} setShowButton={setShowButton} tittle={tittle} />
-
-                            {/* para probar directamente en React NO esta generado por la Base de datos */}
-                            <div className="divRectangulos">
-                                <div>
-                                    <img className="rectanguloBase" src={rectanguloFondo} alt="tablaFondo"></img>
-                                    <div className="divIzq">
-                                        <img className="imagenTablaH" src={tablaSurf2H} alt="tablaSurf"></img>
-                                        <p className="rectanguloTexto">Sin neopreno - 1 día</p>
-                                    </div>
-
-                                </div>
-                                <div>
-                                    <img className="rectanguloBase" src={rectanguloFondo} alt="tablaFondo"></img>
-                                    <div className="divIzq">
-                                        <img className="imagenTablaH" src={tablaSurf9H} alt="tablaSurf"></img>
-                                        <p className="rectanguloTexto">Sin neopreno - 2 días</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <img className="rectanguloBase" src={rectanguloFondo} alt="tablaFondo"></img>
-                                    <div className="divIzq">
-                                        <img className="imagenTablaH" src={tablaSurf12H} alt="tablaSurf"></img>
-                                        <p className="rectanguloTexto">Sin neopreno - 5 días</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <img className="rectanguloBase" src={rectanguloFondo} alt="tablaFondo"></img>
-                                    <div className="divIzq">
-                                        <img className="imagenTablaH" src={tablaSurf11H} alt="tablaSurf"></img>
-                                        <p className="rectanguloTexto">Sin neopreno - 7 días</p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div className="divDer">
-                            <div>
-                                <img className="imagenTablaH" src={tablaSurf2} alt="tablaSurf"></img>
-                            </div>
-                            <div className="textoDerecha">
-                                <p className="tituloDerecha">Tabla de Surf </p>
-                                <p className="tituloDerecha">{tittle} accesorios</p>
-                                <p>-</p>
-                                <h2>5 días - 28 €</h2>
-                                
-                                <ArticuloDetallado articuloFull={articuloFull} showButton={showButton} />
-                                <button class="buttonGeneral"><a href="./carrito.html">Añadir al Carro</a></button>
-                                <div class="textoSurf textoDescripcion">
-                                    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                                        Eveniet modi neque id officiis commodi earum repellat,
-                                        totam nam iusto ea sed cumque dolorum ab numquam laudantium provident delectus.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div>
+                        <h2>Alquiler tablas de Surf {tittle} neopreno</h2>
                     </div>
-
+                    <ListaArticulos host={pag} tittle={tittle} setArticuloFull={setArticuloFull} setShowButton={setShowButton} />
+                    <ArticuloDetallado articuloFull={articuloFull} showButton={showButton} />
                 </div>
-        </main>
-     </>
+            </div>
+
+        </>
     )
 }
 
