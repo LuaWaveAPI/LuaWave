a5c2f7c94630ad964648fe69c90e0ff98b8d526a
Maitlla <ohmelopido@gmail.com>
2022-06-22 13:57:29 +0200

    A ver 2

---
+++ frontend/src/components/LogoGrande/LogoGrande.jsx
@@ -0,0 +1,10 @@
+import imagen from "../../img/logoLuaWave.png"
+export function LogoGrande(){
+    return(
+        <>
+        <div>
+            <img src={imagen} alt="hola" />
+        </div>
+        </>
+    )
+}
