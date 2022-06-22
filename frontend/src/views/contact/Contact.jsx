aa0f5e7e6f43d41904e4e000c073cc1cb2aacc47
KhrisDV <KhrisQ@protonmail.com>
2022-06-22 12:43:53 +0200

    contact form

--- frontend/src/views/contact/Contact.jsx
+++ frontend/src/views/contact/Contact.jsx
@@ -1,18 +1,20 @@
 import style from "../Contact/Contact.module.css"
 
 function Contact () {
+    
+
     return (
         <>
         <div className="contact">
         <h1>Contáctanos</h1>
-        <form>
+        <form action="http://localhost:4000/api/v0.0/contact/" method="POST" target="_blank">
         <input type="text" placeholder="Nombre" name="name" id="inputname" />
         <br />
         <input type="email" placeholder="Correo eléctrónico" name="mail" id="inputmail" />
         <br />
         <textarea name="coment" placeholder="Haznos llegar tu mensaje" id="areacoment" cols="30" rows="10"></textarea>
         <br />
-        <button className="submit" type="button">Enviar</button>
+        <button className="submit" type="submit">Enviar</button>
         </form>
         </div>
         </>
